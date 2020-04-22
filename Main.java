6a724c6d09fd696ba0e5bd28241bfa488078be78
Radu-Tiberiu CRISTIAN <63586497+CristianTiberiu@users.noreply.github.com>
2020-04-22 17:24:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package homeWorkC8;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Vehicle Tibys = new Vehicle("A241fg1", 4, "Joiana");
+
+		Tibys.goTo(12.8, 24.9);
+		Tibys.addFuel(30.5);
+		Tibys.printInfo();
+
+		OnRoad aVehicleOnRoad = new OnRoad("A2b2234gfF", 4, "Mario");
+
+		aVehicleOnRoad.goTo(2.3, 1.8);
+		aVehicleOnRoad.addFuel(38);
+		aVehicleOnRoad.printInfo();
+
+		OnWater aVasseOnWater = new OnWater("New2Boat", 80, "The Tank", 3, 2000.5);
+
+		aVasseOnWater.goTo(12.5, 129.48);
+		aVasseOnWater.addFuel(2530);
+		aVasseOnWater.printInfo();
+
+		OnAir aPlaneOnAir = new OnAir("A2z432b4", 320, "The Sonic", 6, true);
+
+		aPlaneOnAir.goTo(1246.8, 2541.4);
+		aPlaneOnAir.addFuel(14500.8);
+		aPlaneOnAir.printInfo();
+
+	}
+
+}
